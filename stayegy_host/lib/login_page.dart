19b88cb25ae5bc87714804494fdbb950c31fc341
Mehedi Hasan Emo <hasanemo654@gmail.com>
2020-12-28 17:27:13 +0600

    host_app_work

---
+++ stayegy_host/lib/login_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class login_page extends StatefulWidget {
+  @override
+  _login_pageState createState() => _login_pageState();
+}
+
+class _login_pageState extends State<login_page> {
+  String _counntryCode, _phoneNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            //Container for logo
+            Container(
+              alignment: Alignment.topCenter,
+              padding: const EdgeInsets.fromLTRB(90, 200, 70, 0),
+              child: Image.asset('images/stayegy host logo.png'),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height / 2),
+                height: 358,
+                padding: EdgeInsets.fromLTRB(30, 150, 30, 150),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.5,
+                    ),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Image.asset(
+                            'images/BD Flag.png',
+                            scale: 100,
+                          ),
+                        ),
+                      ),
+                      VerticalDivider(
+                        color: Colors.blueGrey,
+                        thickness: 2,
+                        width: 2,
+                        indent: 8,
+                        endIndent: 8,
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.only(left: 10),
+                          child: TextField(
+                            keyboardType: TextInputType.phone,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            onChanged: (value) {
+                              _phoneNumber = value;
+                            },
+
+                            /// TODO: Decorate the textfield perfectly
+                            decoration: InputDecoration(
+                              hintText: 'Enter Number',
+                              disabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.grey,
+                          margin: EdgeInsets.only(left: 15),
+                          child: GestureDetector(
+                            child: Image.asset(
+                              'images/next button.png',
+                              scale: 2.5,
+                              //color: Colors.black,
+                            ),
+                            onTap: () {
+                              print('Pressed!');
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ stayegy_host/lib/otp_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/style.dart';
+import 'package:stayegy_host/bottom_button.dart';
+
+class otp_page extends StatefulWidget {
+  @override
+  _otp_pageState createState() => _otp_pageState();
+}
+
+// ignore: camel_case_types
+class _otp_pageState extends State<otp_page> {
+  String otp;
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController otp_getter = new TextEditingController();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.topCenter,
+            padding: const EdgeInsets.fromLTRB(90, 200, 70, 0),
+            child: Image.asset('images/stayegy host logo.png'),
+          ),
+          SizedBox(
+            height: 80,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            // OTPTextField is a dart package: "otp_text_field: ^1.0.1"
+            // Link: https://pub.dev/packages/otp_text_field
+            child: OTPTextField(
+              length: 6,
+              width: MediaQuery.of(context).size.width,
+              fieldWidth: 35,
+              style: TextStyle(fontSize: 35),
+              textFieldAlignment: MainAxisAlignment.spaceAround,
+              fieldStyle: FieldStyle.underline,
+              onCompleted: (code) {
+                otp = code;
+                print("Entered otp: " + code);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+            child: BottomButton(
+              text: 'SUBMIT',
+              disabled: false,
+              onTap: () {
+                FocusScope.of(context).unfocus();
+                print(otp);
+              },
+            ),
+          ),
+          Text(
+            "Didn't receive the code? Resend the code in 00:59",
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
